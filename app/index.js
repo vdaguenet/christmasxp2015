23fe63240d5893c10bddc796cdb158a797bba6f3
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-11-30 22:18:14 +0100

    loader

--- app/index.js
+++ app/index.js
@@ -8,8 +8,9 @@ import PatternPicker from './components/PatternPicker';
 
 let webgl;
 
-Mediator.once('preload:complete', main);
+loaderAnim();
 
+Mediator.once('preload:complete', main);
 PreloaderInterface.load([
   { id: 'gift', src: 'assets/patterns/gift.png' },
   { id: 'holy', src: 'assets/patterns/holy.png' },
@@ -25,12 +26,38 @@ PreloaderInterface.load([
   { id: 'preview-reindeer', src: 'assets/previews/reindeer.png' },
   { id: 'template', src: 'assets/print-template.png' }
 ]);
-
 PreloaderInterface.loadTextures([
   { id: 'mcap3', src: 'assets/textures/mcap3.png' }
 ]);
 
-function bindEvents() {
+function loaderAnim() {
+  [].forEach.call(document.querySelectorAll('#snowball circle'), ($circle, i) => {
+    TweenMax.fromTo($circle, 0.8, { y: -100 * Math.random(), alpha: 0.5 },
+      { y: 60, alpha: 1, repeat: -1, delay: Math.random() * i * 0.3, ease: Linear.easeNone })
+  });
+}
+
+function loaderTransitionOut() {
+  const start = 1.0;
+  const $loaderEls = document.querySelectorAll('.loader-content h1, .loader-content svg, .loader-content p')
+  const $loader = document.querySelector('.loader');
+  const $selectors = document.querySelectorAll('.picker p, .color-picker, .pattern-picker, .button');
+
+  const tl = new TimelineMax();
+  tl.staggerFromTo($loaderEls, 0.8, { y: 0 }, { y: 50, alpha: 0, ease: Expo.easeInOut }, -0.08, start);
+  tl.fromTo($loader, 0.8, { scaleX: 1 }, { scaleX: 0.32, ease: Expo.easeInOut }, start + 0.7);
+  tl.to($loader, 0.4, { autoAlpha: 0, display: 'none' }, start + 1.3);
+  tl.staggerFromTo($selectors, 0.9,
+    { y: 50, alpha: 0 },
+    { y: 0, alpha: 1, ease: Expo.easeOut }, 0.08, start + 1.3);
+  tl.fromTo(webgl.ball.scale, 4.0,
+    {x: 0.1, y: 0.1, z: 0.1},
+    {x: 1.0, y: 1.0, z: 1.0, ease: Expo.easeOut}, start);
+  tl.fromTo(webgl.ball.rotation, 4.0,
+    {y: -Math.PI}, {y: Math.PI * -0.08, ease: Expo.easeOut}, start);
+}
+
+function initComponents() {
   const bulbColorPick = new ColorPicker(document.querySelector('#bulbColor'), 'bulb', webgl.ball.availableColors, webgl.ball.color);
   const patternColorPick = new ColorPicker(document.querySelector('#patternColor'), 'pattern', webgl.ball.availablePatternColors, webgl.ball.patternColor);
   const patternTopPicker = new PatternPicker(document.querySelector('#patternTop'), 'top', webgl.ball.availblePatterns, webgl.ball.patternTop);
@@ -55,10 +82,12 @@ function animate() {
 function main() {
   webgl = new Webgl(window.innerWidth, window.innerHeight);
   document.body.appendChild(webgl.renderer.domElement);
-  // events
-  bindEvents();
+  // dom components
+  initComponents();
   // handle resize
   window.addEventListener('resize', resizeHandler);
   // let's play !
   animate();
+
+  loaderTransitionOut();
 }
